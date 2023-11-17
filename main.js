47e965f76085115e376a0acfdd5c1ad224049bd2
Wevertonguesso <103575237+Wevertonguesso@users.noreply.github.com>
2023-11-17 10:41:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+//pom
 function tocasonpom (){
-   document.querySelector(‘#som_tecla_pom’).play();
+   document.querySelector('#som_tecla_pom').play();
 }
     
-document.querySelector(‘.tecla_pom’);
+// aqui o codigo faz tal coisa.
+document.querySelector('tecla_pom').play(); 
+document.querySelector('.teclA_POM').onclick= tocasonpom;
+
+//clap
+function tocasonclap() {
+     document. querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick=tocasonclap;
+
+//tim
+function tocasontim() {
+   document. querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick=tocasonclap;
+
+//puff
+function tocasonpuff() {
+   document. querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick=tocasonclap;
+
+//splash
+function tocasonsplash() {
+   document. querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick=tocasonclap;
+
+//toim
+function tocasontoim() {
+   document. querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick=tocasonclap;
+
+//psh
+function tocasonpsh() {
+   document. querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick=tocasonclap;
+
+//tic
+function tocasontic() {
+   document. querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick=tocasonclap;
+
+//tom
+function tocasontom() {
+   document. querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick=tocasonclap
